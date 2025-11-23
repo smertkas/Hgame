d9f55540c53fb5819cc713d7a1a6729f6f6e6dc2
smertkas <smertkas895@gmail.com>
2025-11-23 14:04:30 +0300

    Новая комната, но без переходов, FrameSwitch пока что неимеет никакого значения

--- Assets/Scripts/FrameSwitch.cs
+++ Assets/Scripts/FrameSwitch.cs
@@ -5,14 +5,14 @@ public class FrameSwitch : MonoBehaviour
     public GameObject activeFrame;
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Plat"))
+        if (other.CompareTag("Player"))
         {
             activeFrame.SetActive(true);
         }
     }
     private void OnTriggerExit2D(Collider2D other)
     {
-        if (other.CompareTag("Plat"))
+        if (other.CompareTag("Player"))
         {
             activeFrame.SetActive(false);
         }
